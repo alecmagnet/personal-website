bcea6fcc185302388fab2e885df3ff119e895683
Alec Magnet <amagnet@gmail.com>
2021-12-01 02:42:42 -0500

    wireframe draft

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,45 @@
-import { Typography, Grid, Box, } from '@mui/material'
+import { Grid, Box, Button, Divider, } from '@mui/material'
 import { Link } from 'react-router-dom'
 
 export default function Navbar() {
 
-	const links = ['Projects', 'Bio', 'Resume', 'Academic']
-
-	let subTitleSx = {
-		fontWeight: 'light', 
-		fontSize: 'h3.fontSize',
-	}
-
+	const links = ['projects', 'bio', 'resume', 'academic']
 
 
 	return (
-			<Grid container columnSpacing={2} justifyContent="center" >
+			<Grid container columnSpacing={2} justifyContent="center">
 			<Grid item xs="auto">
 				<Box component="span" sx={{ 
 				fontWeight: "bold", 
 				fontSize: 'h3.fontSize',
-				pb:0, mb:0 
 			}}>
 				Alec Magnet 
 			</Box>
 			</Grid>
 			<Grid item xs="auto">
-				<Box component="span" sx={subTitleSx}> is a software engineer</Box>
+				<Box component="span" sx={{ 
+				fontWeight: "light", 
+				fontSize: 'h3.fontSize',
+			}}> 
+					is a software engineer
+				</Box>
 			</Grid>
-			<Grid item xs={12}>
-
+			<Grid item container xs={12} justifyContent="center" sx={{ mt:-1 }}>
+					{links.map(el => 
+						<>
+							<Button variant="text" sx={{ px: 2, mx: 2, height: 50, color: "text.secondary" }}>
+								{el}
+							</Button>
+							{links.indexOf(el) === links.length - 1 ?
+								null
+								: <Divider orientation="vertical" flexItem />	
+							}
+						</>	
+					)}
 			</Grid>
+			{/* <Grid item container xs={12} justifyContent="center" sx={{ mt:1 }}>
+				<Divider width="75%" />
+			</Grid> */}
 		</Grid>
 	)
 }
