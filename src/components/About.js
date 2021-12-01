df02c88de7d6d1e32d8e8793997bdc86ac094439
Alec Magnet <amagnet@gmail.com>
2021-12-01 02:57:11 -0500

    added headings

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,13 @@
+import { Typography } from '@mui/material'
 
 
 export default function About() {
 
 	return (
 		<>
+			<Typography variant="h2" align="center" sx={{ letterSpacing: "7px"}}>
+				Who Am I?
+			</Typography>
 		</>
 	)
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,13 @@
+import { Typography } from '@mui/material'
 
 
 export default function Projects() {
 
 	return (
 		<>
+			<Typography variant="h2" align="center" sx={{ letterSpacing: "7px"}}>
+				Projects
+			</Typography>
 		</>
 	)
 }
