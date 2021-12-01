a1688d9e2b0b12cebbf7bf4fc16117a157fa9a3f
Alec Magnet <amagnet@gmail.com>
2021-12-01 15:52:57 -0500

    added useEffect to scroll to location hash -- does it work?

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,19 +1,36 @@
+import { useRef, useEffect } from "react"
+import { useLocation } from 'react-router-dom'
+import { Grid } from '@mui/material'
 import Projects from "./Projects"
 import About from "./About"
 
 export default function Homepage() {
+	const location = useLocation()
+	const reference = useRef()
+
+	useEffect(() => {
+		if (location.hash.length > 0) reference.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
+	}, [location, reference])
+
+	const isHashElem = (id) => {if (location.hash.includes(id)) return <div ref={reference}/>}
 
 	return (
-		<>
+		<Grid container spacing={2}>
 
-			<div id="projects">
-				<Projects />
-			</div>
+			{isHashElem('projects')}
+			<Grid id="projects" item container sx={12} justifyContent="center">
+				<div >
+					<Projects />
+				</div>
+			</Grid>
 
-			<div id="about">
-				<About />
-			</div>
+			{isHashElem('about')}
+			<Grid id="about" item container sx={12} justifyContent="center">
+				<div >
+					<About />
+				</div>
+			</Grid>
 
-		</>
+		</Grid>
 	)
 }
